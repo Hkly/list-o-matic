e5df681f2603ee5533d3bd877e45ee39b21f3688
Hannah Yiu <hannahkyiu@gmail.com>
2014-07-21 12:02:34 -0700

    add cards collection/model

--- app/assets/javascripts/models/list.js
+++ app/assets/javascripts/models/list.js
@@ -1,3 +1,17 @@
 TrelloClone.Models.List = Backbone.Model.extend({
-  urlRoot: "api/lists"
+  urlRoot: "api/lists",
+
+  cards: function() {
+    if (!this._cards) {
+      this._cards = new TrellowClone.Collections.Cards([], {list: this});
+    }
+    return this._cards;
+  },
+
+  parse: function(jsonResponse) {
+    if (jsonResponse.cards) {
+      this.cards().set(jsonResponse.cards, {parse: true});
+    }
+    return jsonResponse;
+  }
 });
